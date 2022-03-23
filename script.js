0c078fbcf81b58e5469638b94778453599bf868a
TS93 <tschram93@yahoo.com>
2022-03-23 16:32:28 -0400

    color gradient extends based on mouse position in y axis

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ const video = document.querySelector('.flex');
 speed.addEventListener('mousemove', function(e) {
     const y = e.pageY - this.offsetTop;
     const percent = y / this.offsetHeight;
+    const min = 0.4;
+    const max = 4;
+    const height = Math.round(percent * 100) + '%';
+    bar.style.height = height;
     console.log(percent)
 }
 );
