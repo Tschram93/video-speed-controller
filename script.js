2a671a822e0128d46dd6fb92c6197d503d469b2d
TS93 <tschram93@yahoo.com>
2022-03-23 16:28:07 -0400

    creating percent of speedbar based of y axis positioning

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ const speed = document.querySelector('.speed');
 
 const video = document.querySelector('.flex');
     console.log(video);
+
+// Event Listeners
+speed.addEventListener('mousemove', function(e) {
+    const y = e.pageY - this.offsetTop;
+    const percent = y / this.offsetHeight;
+    console.log(percent)
+}
+);
