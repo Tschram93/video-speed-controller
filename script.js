45663df2c8f7d64131301122eb8eece5e3a659be
TS93 <tschram93@yahoo.com>
2022-03-23 16:17:00 -0400

    creating variables for selectors

---
+++ script.js
@@ -0,0 +1,10 @@
+// Selectors
+
+const bar = document.querySelector('.speed-bar');
+    console.log(bar);
+
+const speed = document.querySelector('.speed');
+    console.log(speed);
+
+const video = document.querySelector('.flex');
+    console.log(video);
